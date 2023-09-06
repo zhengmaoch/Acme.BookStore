a2c458ed8e19038f3ce086a499648cead8508966
Chang <zhengmaoch@msn.com>
2023-09-06 14:38:32 +0800

    add permissions

--- src/Acme.BookStore.Application.Contracts/Permissions/BookStorePermissions.cs
+++ src/Acme.BookStore.Application.Contracts/Permissions/BookStorePermissions.cs
@@ -6,4 +6,12 @@ public static class BookStorePermissions
 
     //Add your own permission names. Example:
     //public const string MyPermission1 = GroupName + ".MyPermission1";
+
+    public static class Books
+    {
+        public const string Default = GroupName + ".Books";
+        public const string Create = GroupName + ".Create";
+        public const string Edit = GroupName + ".Edit";
+        public const string Delete = GroupName + ".Delete";
+    }
 }
